3b475ef07767c0dbfaeeb6c49df888db38f266c3
IceBear05 <hawstix.nz@gmail.com>
2023-07-04 22:59:40 +0330

    menu and database

--- login.py
+++ login.py
@@ -1,79 +1,79 @@
-# class LoginAndSignUp:
-#     def __init__(self):
-#         pass
-#
-#     def Check(self, user, password):
-#         File = open("users.txt", "r")
-#         for line in File:
-#             if len(line) < 3:
-#                 continue
-#             us, pss = line.split(" ")
-#             if user == us and int(password) == int(pss):
-#                 return False
-#         return True
-#
-#     def SignUp(self, user, password):
-#         if self.Check(user, password):
-#             File = open("users.txt", "a")
-#             File.writelines([user, " ", password, "\n"])
-#             return True
-#         else:
-#             return False
-#
-#     def Login(self, user, password):
-#         File = open("users.txt", "r")
-#         for line in File:
-#             if len(line) < 3:
-#                 continue
-#             us, pss = line.split(" ")
-#             if user == us and int(password) == int(pss):
-#                 return True
-#         return False
-#
-#
-# o = LoginAndSignUp()
-# print("------------------------- Welcome ------------------------")
-# while True:
-#     print("1.[Login] \t\t\t\t2.[SignUp] \t\t\t\t", end='')
-#     choice = input("3.[Exit]  :")
-#
-#     if choice == "1":
-#         print("-------------------------- Login -------------------------")
-#         user = input("please enter Username: ")
-#         password = input("please enter password: ")
-#
-#         if o.Login(user, password):
-#             print("---------------------- Welcome To App ---------------------")
-#             break
-#         else:
-#             print("Username and password are not correct\n")
-#             continue
-#
-#     elif choice == "2":
-#         print("------------------------- SignUp ------------------------")
-#         user = input("please enter Username: ")
-#         password = input("please enter password: ")
-#
-#         if o.SignUp(user, password):
-#             print("-------------- Registration was successful --------------")
-#             print("---------------------- Welcome To App ---------------------")
-#             break
-#         else:
-#             print("The user already exists\n")
-#             continue
-#
-#     elif choice == "3":
-#         print("------------------------- GoodBye ------------------------")
-#         break
-#     else:
-#         print("-------------------------- ERROR -------------------------")
-#         print("Please enter [1 or 2 or 3]")
-#         print("----------------------------------------------------------")
-#         continue
+class LoginAndSignUp:
+    def __init__(self):
+        pass
+
+    def Check(self, user, password):
+        File = open("users.txt", "r")
+        for line in File:
+            if len(line) < 3:
+                continue
+            us, pss = line.split(" ")
+            if user == us and int(password) == int(pss):
+                return False
+        return True
+
+    def SignUp(self, user, password):
+        if self.Check(user, password):
+            File = open("users.txt", "a")
+            File.writelines([user, " ", password, "\n"])
+            return True
+        else:
+            return False
+
+    def Login(self, user, password):
+        File = open("users.txt", "r")
+        for line in File:
+            if len(line) < 3:
+                continue
+            us, pss = line.split(" ")
+            if user == us and int(password) == int(pss):
+                return True
+        return False
+
+
+o = LoginAndSignUp()
+print("------------------------- Welcome ------------------------")
+while True:
+    print("1.[Login] \t\t\t\t2.[SignUp] \t\t\t\t", end='')
+    choice = input("3.[Exit]  :")
+
+    if choice == "1":
+        print("-------------------------- Login -------------------------")
+        user = input("please enter Username: ")
+        password = input("please enter password: ")
+
+        if o.Login(user, password):
+            print("---------------------- Welcome To App ---------------------")
+            break
+        else:
+            print("Username and password are not correct\n")
+            continue
+
+    elif choice == "2":
+        print("------------------------- SignUp ------------------------")
+        user = input("please enter Username: ")
+        password = input("please enter password: ")
+
+        if o.SignUp(user, password):
+            print("-------------- Registration was successful --------------")
+            print("---------------------- Welcome To App ---------------------")
+            break
+        else:
+            print("The user already exists\n")
+            continue
+
+    elif choice == "3":
+        print("------------------------- GoodBye ------------------------")
+        break
+    else:
+        print("-------------------------- ERROR -------------------------")
+        print("Please enter [1 or 2 or 3]")
+        print("----------------------------------------------------------")
+        continue
 
 
 
-'''from mysql.connector import connect
+from mysql.connector import connect
 
 db = connect(
             host="localhost",
@@ -84,7 +84,7 @@ db = connect(
 my_cursor = db.cursor()
 my_cursor.execute(f"SELECT * FROM Loading")
 for line in my_cursor:
-    print(line)'''
+    print(line)
 
 def MainMenu():
     print('_______________________management menu_______________________')
@@ -118,5 +118,7 @@ def LoadingMenu():
 def SendAndReceiveMenu():
     print('____________________send & receive menu____________________')
     print('1.[waiting boxes]')
-    choice = input('2.[bill of lading :')
+    choice = input('2.[bill of lading] :')
     return choice
+
+SendAndReceiveMenu()
